6a1ed2aadf5d3a07fd0635dde1a9618eae2bf772
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-27 10:28:33 +0530

    Add dynamic routing for individual cities from Url using react-routers

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000/cities";
 
@@ -48,7 +49,8 @@ function App() {
               path="cities"
               element={<CityList cities={cities} isLoading={loading} />}
             />
-            <Route path="countries" element={<CountryList cities={cities}/>} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList cities={cities} />} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="*" element={<Pagenotfound />} />
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+
+
 function CityItem({ city }) {
-  // console.log(city)
+  
+
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
       day: "numeric",
@@ -9,14 +13,16 @@ function CityItem({ city }) {
       weekday: "long",
     }).format(new Date(date));
 
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.time}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link to={`${id}`} className={styles.cityItem}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.time}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
